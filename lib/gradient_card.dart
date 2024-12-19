08f3d7a3a41a96bda21b87e4c32999c6a05419b4
Nafeesa-Yousaf <nafeesayousaf2129@gmail.com>
2024-12-19 22:30:39 +0500

    initial commit

---
+++ lib/gradient_card.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GradientCard extends StatelessWidget {
+  final String quote;
+  final List<Color> gradientColors;
+  final String backgroundImage;
+  final Color quoteColor;
+
+  const GradientCard({
+    Key? key,
+    required this.quote,
+    required this.gradientColors,
+    required this.backgroundImage,
+    required this.quoteColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        elevation: 10,
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: gradientColors,
+            ),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Stack(
+            children: [
+              // Add a background image with opacity
+              Opacity(
+                opacity: 0.6,
+                child: Image.asset(
+                  backgroundImage,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Center(
+                child: Text(
+                  quote,
+                  style: GoogleFonts.pacifico(
+                    textStyle: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: quoteColor,
+                      letterSpacing: 1.5,
+                      height: 1.5,
+                      shadows: [
+                        Shadow(
+                          offset: Offset(3.0, 3.0),
+                          blurRadius: 6.0,
+                          color:
+                              Color.fromARGB(255, 83, 82, 82).withOpacity(0.4),
+                        ),
+                      ],
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MainCard extends StatelessWidget {
+  const MainCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        elevation: 10,
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromRGBO(249, 21, 215, 1),
+                Color.fromRGBO(22, 0, 98, 1)
+              ],
+            ),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Center(
+            child: Image.asset(
+              'assets/image.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,137 +1,213 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:flutter_animation/gradient_card.dart';
+import 'package:scrumlab_flutter_tindercard/scrumlab_flutter_tindercard.dart';
 
-class FlowerShapeClipper extends CustomClipper<Path> {
-  final double petalLength = 40.0;
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
-  Path getClip(Size size) {
-    final path = Path();
-
-    for (double angle = 0; angle < 2 * pi; angle += pi / 3) {
-      double x = size.width / 2 + petalLength * cos(angle);
-      double y = size.height / 2 + petalLength * sin(angle);
-
-      path.moveTo(size.width / 2, size.height / 2);
-      path.quadraticBezierTo(size.width / 2, size.height / 2 - petalLength / 2, x, y);
-      path.quadraticBezierTo(size.width / 2, size.height / 2 + petalLength / 2, size.width / 2, size.height / 2);
-    }
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SwipCardAnimation(),
+    );
   }
 }
 
-class ColorShapeSlider extends StatefulWidget {
+class SwipCardAnimation extends StatefulWidget {
+  const SwipCardAnimation({super.key});
+
   @override
-  _ColorShapeSliderState createState() => _ColorShapeSliderState();
+  State<SwipCardAnimation> createState() => _SwipCardAnimationState();
 }
 
-class _ColorShapeSliderState extends State<ColorShapeSlider> {
-  double _sliderValue = 0.5;
+class _SwipCardAnimationState extends State<SwipCardAnimation>
+    with TickerProviderStateMixin {
+  // Colors and gradients for reusable styling
+  final Color backgroundColor = const Color.fromARGB(255, 96, 15, 109);
+  final List<Widget> cards = _generateCards();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
 
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [calculateProfessionalColor(_sliderValue), Colors.white], // Blue to White gradient
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedContainer(
-                duration: Duration(milliseconds: 500),
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  color: calculateProfessionalColor(_sliderValue),
-                  shape: _calculateShape(),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.3),
-                      spreadRadius: 3,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: ClipPath(
-                  clipper: FlowerShapeClipper(),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [_calculatePetalColor(), Colors.white],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Slider(
-                value: _sliderValue,
-                onChanged: (value) {
-                  setState(() {
-                    _sliderValue = value;
-                  });
-                },
-              ),
-        SizedBox(height: 20),
-        CircleAvatar(
-          radius: 40,
-          backgroundColor: Colors.white,
-          child: Text(
-            'H',
-            style: TextStyle(
-              fontSize: 30,
-              color: calculateProfessionalColor(_sliderValue),
-            ),
-          ),
-        ),
-        SizedBox(height: 10),
-        Text(
-          'Hello Hassan',
-          style: TextStyle(
-            fontSize: 20,
-            color: _calculatePetalColor(),
-          ),)
-            ],
-          ),
-        ),
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: [
+          _buildDecorativeImages(width, height),
+          _finalScreen(),
+          _buildCardStack(width, height),
+        ],
       ),
     );
   }
 
-  Color calculateProfessionalColor(double sliderValue) {
-    return Color.lerp(Color(0xFF0F7CE8), Color(0xFF67AEAF), sliderValue) ?? Color(
-        0xFF1E3146);
+  Widget _finalScreen() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+          ),
+          Image.asset(
+            "assets/logo.png",
+            width: 200,
+            height: 200,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Text(
+            "Best wishes for the year ahead",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 255, 255, 255),
+              shadows: [
+                BoxShadow(
+                  color: Colors.purpleAccent.withOpacity(0.6), // Outer glow
+                  blurRadius: 20,
+                  spreadRadius: 5,
+                ),
+                BoxShadow(
+                  color: Colors.purpleAccent.withOpacity(0.4), // Inner glow
+                  blurRadius: 40,
+                  spreadRadius: 20,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Color _calculatePetalColor() {
-    return Color.lerp(Color(0xFFF39C12), Color(0xFFE67E22), _sliderValue) ?? Color(0xFFF39C12); // Orange to Dark Orange
+  // Method to generate the list of cards
+  static List<Widget> _generateCards() {
+    return [
+   const  MainCard(),  const GradientCard(
+        quote:
+            "Every new year is a blank canvas, waiting for you to create a masterpiece.🌟 Set your goals, dream big, and achieve greatness.",
+        gradientColors: [
+          Color.fromRGBO(112, 246, 255, 0.33),
+          Color.fromRGBO(221, 108, 241, 0.26),
+          Color.fromRGBO(229, 106, 253, 0.71),
+          Color.fromRGBO(123, 183, 253, 1),
+        ],
+        backgroundImage: "assets/card1_bg.png",
+        quoteColor: Colors.purple,
+      ),
+      const GradientCard(
+        quote:
+            "This year, let us strive to be the architects of our own destiny. With vision, hard work, and perseverance, we can turn our dreams into reality 🌟",
+        gradientColors: [
+          Color.fromRGBO(252, 37, 103, 1),
+          Color.fromRGBO(250, 38, 151, 1),
+          Color.fromRGBO(186, 8, 181, 1),
+        ],
+        backgroundImage: "assets/card1_bg.png",
+        quoteColor: Color.fromARGB(255, 236, 184, 245),
+      ),
+      
+      const GradientCard(
+        quote:
+            "Embrace the magic of new beginnings. The New Year is a blank canvas; paint it with your dreams, aspirations, and unwavering belief in yourself. 🎨",
+        gradientColors: [
+          Color.fromRGBO(226, 66, 249, 0.94),
+          Color.fromRGBO(79, 147, 249, 1),
+        ],
+        backgroundImage: "assets/card1_bg.png",
+        quoteColor: Color.fromARGB(255, 186, 228, 255),
+      ),
+      const GradientCard(
+        quote:
+            "As the New Year unfolds, remember that you have the power to shape your destiny. Take charge and make this year your best yet ✨",
+        gradientColors: [
+          Color.fromRGBO(245, 116, 185, 1),
+          Color.fromRGBO(89, 97, 223, 1),
+        ],
+        backgroundImage: "assets/card1_bg.png",
+        quoteColor: Color.fromARGB(255, 255, 206, 177),
+      ),
+      const GradientCard(
+        quote:
+            "This year, let’s prioritize self-care and personal growth. A fulfilled life is built on nurturing our passions and embracing our true selves. ❤",
+        gradientColors: [
+          Color.fromRGBO(254, 122, 152, 0.81),
+          Color.fromRGBO(255, 206, 134, 1),
+          Color.fromRGBO(172, 253, 163, 0.64),
+        ],
+        backgroundImage: "assets/card1_bg.png",
+        quoteColor: Colors.purple,
+      ),
+    ];
   }
 
-
-  BoxShape _calculateShape() {
-    return _sliderValue > 0.5 ? BoxShape.circle : BoxShape.rectangle;
+  // Method to build the decorative images in the background
+  Widget _buildDecorativeImages(double width, double height) {
+    return Stack(
+      children: [
+        Positioned(
+          right: -15,
+          child: Image.asset(
+            "assets/top_right.png",
+            width: width * 0.5,
+            height: height * 0.3,
+          ),
+        ),
+        Positioned(
+          left: -15,
+          top: -10,
+          child: Image.asset(
+            "assets/top_left.png",
+            width: width * 0.5,
+            height: height * 0.3,
+          ),
+        ),
+      ],
+    );
   }
-}
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ColorShapeSlider(),
-  ));
+  // Method to build the card stack
+  Widget _buildCardStack(double width, double height) {
+    return Column(
+      children: [
+        SizedBox(height: height * 0.2),
+        SizedBox(
+          height: height * 0.6,
+          child: TinderSwapCard(
+            swipeUp: true,
+            swipeDown: true,
+            orientation: AmassOrientation.bottom,
+            totalNum: cards.length,
+            stackNum: 4,
+            swipeEdge: 4.0,
+            maxWidth: width * 0.9,
+            maxHeight: width,
+            minWidth: width * 0.8,
+            minHeight: width * 0.8,
+            cardBuilder: (context, index) => cards[index],
+            cardController: CardController(),
+            swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
+              if (align.x < 0) {
+                print("Swiping Left");
+              } else if (align.x > 0) {
+                print("Swiping Right");
+              }
+            },
+            swipeCompleteCallback:
+                (CardSwipeOrientation orientation, int index) {
+              print("Card $index swiped $orientation");
+            },
+          ),
+        ),
+      ],
+    );
+  }
 }
