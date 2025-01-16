89d924b4dd5de5f62619304d28ff5665d2ed7dcf
Nafeesa-Yousaf <nafeesayousaf2129@gmail.com>
2025-01-16 18:42:37 +0500

    call screen in main

---
+++ lib/screens/global_warming_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GlobalWarimgScreen extends StatelessWidget {
+  const GlobalWarimgScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
