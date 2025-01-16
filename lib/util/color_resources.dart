75d4275b3887a7ce0bf2e041e063d74d79d22384
Nafeesa-Yousaf <nafeesayousaf2129@gmail.com>
2025-01-16 18:30:47 +0500

    add colors

---
+++ lib/util/color_resources.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorResources {
+  static Color white = const Color(0xffffffff);
+  static Color primaryColor = const Color(0xffffffff);
+  static Color questionColor = const Color(0xff3c3c3c);
+  static Color red = const Color(0xffff6961);
+  static Color green = const Color(0xff77dd77);
+  static Color orange = const Color(0xffffb347);
+  static Color purple = const Color(0xffc899c9);
+  static Color headingColor = const Color(0xff044230);
+}
